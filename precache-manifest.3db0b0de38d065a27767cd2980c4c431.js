bd8e22daf6df81317ac4bab326050d9cf373c07e
albert_u <albert.u1994@gmail.com>
2019-10-29 19:49:54 +0300

    Updates

---
+++ precache-manifest.3db0b0de38d065a27767cd2980c4c431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c4ec3b6511d0fd71552a2777e54695",
+    "url": "/search/index.html"
+  },
+  {
+    "revision": "5691e894d961b639f724",
+    "url": "/search/static/css/main.d39ec68b.chunk.css"
+  },
+  {
+    "revision": "4df3d17874cdfd57883b",
+    "url": "/search/static/js/2.ca4150e2.chunk.js"
+  },
+  {
+    "revision": "5691e894d961b639f724",
+    "url": "/search/static/js/main.06835826.chunk.js"
+  },
+  {
+    "revision": "a43869fe23274f0bf116",
+    "url": "/search/static/js/runtime-main.52c92d47.js"
+  }
+]);
